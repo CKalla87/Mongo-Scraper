5683be18d082536c150c3b6b63ee04c11fbef016
Chris Kalla <ChristopherKalla@MacBook-Pro-2.local>
2018-06-24 16:12:41 -0700

    changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
+var path = require('path');
+var favicon = require('serve-favicon');
 
 //Creating the port connection
 var PORT = process.env.PORT || 8000;
@@ -15,6 +17,11 @@ var routes = require("./routes");
 
 //Setting public route using express
 app.use(express.static("public"));
+
+//setting up favicon middleware
+app.use(favicon(path.join(__dirname, 'public', 'assets/Images/favicon.ico')));
+
+
 
 //Using handlebars with application
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
